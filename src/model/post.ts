00c5a45e412f358c31575e5b6e8f416c6958febc
Beat Besmer <beat.besmer@gmail.com>
2017-05-16 15:14:38 +0200

    Also map image thumbs and squares

--- src/model/post.ts
+++ src/model/post.ts
@@ -2,6 +2,8 @@ export class Post implements Serializable<Post> {
 
   public id: string;
   public image: string;
+  public image_thumb: string;
+  public image_square: string;
   public title: string;
   public url: string;
   public published_at: string;
@@ -10,6 +12,8 @@ export class Post implements Serializable<Post> {
     this.id = input['id'];
     this.title = input['title'];
     this.image = input['image'];
+    this.image_square = input['image_square'];
+    this.image_thumb = input['image_thumb'];
     this.url = input['url'];
     this.published_at = input['published_at'];
     return this;
