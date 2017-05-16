f2122d5842f8a5978165544506c4b8a13ca76893
Beat Besmer <beat.besmer@gmail.com>
2017-05-16 15:15:03 +0200

    Display the images in a grid

--- src/pages/posts/posts.ts
+++ src/pages/posts/posts.ts
@@ -13,13 +13,21 @@ export class PostsPage {
   constructor(public navCtrl: NavController, public navParams: NavParams, public service: DataProvider) {
   }
 
-  public posts: Array<Post>;
+  public posts = Array<Post>();
+
+  chunk(array, size) {
+    let position, end, result = [];
+
+    for (position = 0, end = array.length; position < end; position += size) {
+      let chunk = array.slice(position, position + size);
+      result.push(chunk);
+    }
+    return result;
+  }
 
   ionViewDidLoad() {
     this.service.loadPosts('http://createdaily.ch/nili.json').subscribe(
-      (data) => {
-        this.posts = data;
-      }
+      (posts) => this.posts = posts
     )
   }
 
